54d6319cd81a7fde8bb2e8ad737052725ce18cae
markrp <markrepedersen@gmail.com>
2020-03-15 07:52:05 -0700

    consolidate undo history into single directory...

--- lisp/undo-tree-init.el
+++ lisp/undo-tree-init.el
@@ -4,7 +4,8 @@
   (lambda() (defadvice undo-tree-make-history-save-file-name
 		(after undo-tree activate)
 	      (setq ad-return-value (concat ad-return-value ".gz"))))
-  (setq undo-tree-auto-save-history t))
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo"))))
 
 (global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-S-z") 'undo-tree-redo)
