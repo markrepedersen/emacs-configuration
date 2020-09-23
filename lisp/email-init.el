0a790b991816a2a717f54e73b0242fbceea2df41
Mark Pedersen <markpedersen@MacBook-Pro.hitronhub.home>
2020-09-23 13:41:01 -0700

    Fix issue where moving emails to trash didn't work due to differing path

--- lisp/email-init.el
+++ lisp/email-init.el
@@ -1,41 +1,59 @@
 (use-package mu4e
   :load-path "/usr/local/share/emacs/site-lisp/mu/mu4e"
+  :commands (mu4e mu4e-headers-search mu4e-compose-new mu4e~proc-add)
+  :hook ((mu4e-compose-mode-hook . flycheck-mode)
+	 (mu4e-view-mode-hook . (lambda()
+				  (visual-line-mode)
+				  (local-set-key (kbd "<RET>") 'mu4e~view-browse-url-from-binding)
+				  (local-set-key (kbd "<tab>") 'shr-next-link)
+				  (local-set-key (kbd "<backtab>") 'shr-previous-link))))
   :config
   (setq mail-user-agent 'mu4e-user-agent
+	mu4e-index-cleanup nil ;; don't do a full cleanup check
+	mu4e-index-lazy-check t
+	mu4e-headers-show-threads nil ;; Use "P" to check threads
 	mu4e-mu-binary "/usr/local/bin/mu"
 	message-kill-buffer-on-exit t
 	mu4e-sent-messages-behavior 'delete
-	mu4e-attachment-dir "~/Downloads"
+	mu4e-attachment-dir (expand-file-name "~/Downloads")
 	mu4e-compose-signature-auto-include nil
-	mu4e-drafts-folder "/Gmail/Drafts"
+	mu4e-compose-in-new-frame t
+	mu4e-drafts-folder "/gmail/drafts"
 	mu4e-get-mail-command "mbsync -a"
-	mu4e-maildir "~/Maildir"
-	mu4e-refile-folder "/Gmail/Archive"
-	mu4e-sent-folder "/Gmail/Sent"
-	mu4e-maildir-shortcut
-	'(("/Gmail/INBOX" . ?i)
-	  ("/Gmail/All Mail" . ?a)
-	  ("/Gmail/Deleted" . ?d)
-	  ("/Gmail/Drafts" . ?D)
-	  ("/Gmail/Important" . ?i)
-	  ("/Gmail/Sent Mail" . ?s)
-	  ("/Gmail/Starred" . ?S))
-	mu4e-trash-folder "/Gmail/Trash"
+	mu4e-maildir (expand-file-name "~/mail")
+	mu4e-refile-folder "/gmail/archived"
+	mu4e-sent-folder "/gmail/sent"
+	mu4e-headers-fields '((:human-date .  12) ;; alternatively, use :human-date
+                              ;; (:flags      .   6)
+                              (:from       .  24)
+                              (:subject    .  nil))
+	mu4e-maildir-shortcut '(("/gmail/inbox" . ?i)
+				("/gmail/archived" . ?a)
+				("/gmail/trash" . ?d)
+				("/gmail/drafts" . ?D)
+				("/gmail/important" . ?i)
+				("/gmail/sent" . ?s)
+				("/gmail/starred" . ?S))
+	mu4e-trash-folder "/gmail/trash"
 	mu4e-update-interval 300
 	mu4e-use-fancy-chars t
 	mu4e-view-show-addresses t
+	mu4e-change-filenames-when-moving t
 	mu4e-view-show-images t
 	mu4e-confirm-quit nil
+	mu4e-split-view 'vertical
+	mu4e-headers-skip-duplicates t
+	mu4e-hide-index-messages t
+	org-mu4e-convert-to-html t
 	;; Signature
 	user-mail-address "markrepedersen@gmail.com"
 	user-full-name  "Mark"
 	;; For setting up sending emails
+	message-send-mail-function 'smtpmail-send-it
 	smtpmail-default-smtp-server "smtp.gmail.com"
 	smtpmail-smtp-server "smtp.gmail.com"
 	smtpmail-smtp-service 587
-	mu4e-compose-signature
-	(concat
-	 "Mark Pedersen")))
+	mu4e-compose-signature (concat "Mark Pedersen")))
 
 (use-package mu4e-alert
   :after mu4e
@@ -44,13 +62,14 @@
   :config (mu4e-alert-set-default-style 'libnotify))
 
 (pretty-hydra-define email-functions
-  (:title (with-faicon "toggle-on" "Toggles" 1 -0.05))
+  (:title (with-faicon "toggle-on" "Email" 1 -0.05))
   ("Bookmarks"
    (("s" (mu4e-headers-search) "query")
-    ("u" (mu4e-headers-search "flag:unread AND maildir:/Gmail/INBOX") "unread")
-    ("T" (mu4e-headers-search "date:today..now AND maildir:/Gmail/INBOX") "today")
-    ("W" (mu4e-headers-search "date:1w.. AND maildir:/Gmail/INBOX") "this week")
-    ("M" (mu4e-headers-search "date:1m..  AND maildir:/Gmail/INBOX") "this month"))
+    ("i" (mu4e-headers-search "maildir:/gmail/inbox") "inbox")
+    ("u" (mu4e-headers-search "flag:unread AND maildir:/gmail/inbox") "unread")
+    ("T" (mu4e-headers-search "date:today..now AND maildir:/gmail/inbox") "today")
+    ("W" (mu4e-headers-search "date:1w.. AND maildir:/gmail/inbox") "this week")
+    ("M" (mu4e-headers-search "date:1m..  AND maildir:/gmail/inbox") "this month"))
    "Compose"
    (("C" (mu4e-compose-new) "New"))))
 
